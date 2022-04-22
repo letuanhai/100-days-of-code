ca47f0003aa970f7e28b7a598f9f217e5becead4
letuanhai <letuanhai@live.com>
2022-04-22 23:47:28 +0700

    D26: complete challenge

---
+++ day-26/NATO-alphabet-start/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+df = pd.read_csv(
+    r"C:\Users\hai.letuan\python_projects\100-days-of-code\day-26\NATO-alphabet-start\nato_phonetic_alphabet.csv"
+)
+letter_dict = {row["letter"]: row["code"] for (index, row) in df.iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ")
+
+print([letter_dict[l] for l in user_input.upper()])
