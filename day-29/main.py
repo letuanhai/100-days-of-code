2419b3427e326ef069063874c9c414a2d7eb5b71
letuanhai <letuanhai@live.com>
2022-04-27 00:25:49 +0700

    D30: improve password manager

--- day-29/main.py
+++ day-29/main.py
@@ -2,12 +2,12 @@ import random
 import string
 import tkinter as tk
 from tkinter import messagebox
-import csv
+import json
 
 import pyperclip
 
 DEFAULT_EMAIL = "letuanhai@live.com"
-PW_FILE = "data.csv"
+PW_FILE = "data.json"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -33,6 +33,29 @@ def gen_pw():
     password_entry.insert(0, string=password)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open(PW_FILE, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            pw_data = data[website]
+            messagebox.showinfo(
+                title=website,
+                message=f"Email/Username: {pw_data['email']}\nPassword: {pw_data['password']}",
+            )
+        else:
+            messagebox.showerror(
+                title="Error", message=f"No details for {website} exists."
+            )
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -41,14 +64,29 @@ def save_password():
     email = email_entry.get()
     pw = password_entry.get()
 
+    new_data = {
+        website: {
+            "email": email,
+            "password": pw,
+        }
+    }
+
     if any(len(s) == 0 for s in (website, email, pw)):
         messagebox.showerror(
             title="Oops", message="Please don't leave any field empty!"
         )
     else:
-        with open(PW_FILE, "a", newline="") as f:
-            csv_writer = csv.writer(f, dialect="excel")
-            csv_writer.writerow((website, email, pw))
+        try:
+            with open(PW_FILE, "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            with open(PW_FILE, "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            data.update(new_data)
+            with open(PW_FILE, "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
             website_entry.delete(0, tk.END)
             password_entry.delete(0, tk.END)
 
@@ -65,9 +103,11 @@ canvas.grid(column=1, row=0)
 
 website_label = tk.Label(text="Website:")
 website_label.grid(column=0, row=1)
-website_entry = tk.Entry(width=50)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = tk.Entry(width=30)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
+search_btn = tk.Button(text="Search", width=16, command=find_password)
+search_btn.grid(column=2, row=1)
 
 email_label = tk.Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
